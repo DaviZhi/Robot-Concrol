277806e8e6043e18a662af1e78d0ccad1b16483f
Davi_Zhi <davidwang_0913@qq.com>
2024-12-08 22:18:55 +0800

    some modification

--- Application/DeviceLayer/remote.c
+++ Application/DeviceLayer/remote.c
@@ -1,25 +1,26 @@
 #include "remote.h"
 
-uint8_t Rx_Data[COMMUNI_CYCLE_LENGTH];
+uint8_t Rx_Data[COMMUNI_CYCLE_LENGTH] = {0x00};
+uint8_t Tx_Data[COMMUNI_CYCLE_LENGTH];
 
 /**@brief Rewrite Rx Transfer completed callback function.
 	*@param SPI_HandleTypeDef *hspi
   *@retval None
   */
-void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi)
+void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi)
 {
-	
-//	HAL_SPI_Receive_DMA(&hspi1, Rx_Data, COMMUNI_CYCLE_LENGTH);		// need receive data from remote control continually
+	Remote_Communi_Start();
 }
 
-/**@brief Initialize key of remote control.
-	*@param None
+/**@brief Initialize key info of remote control.
+	*@param Key info structure ptr
   *@retval None
   */
-void RemoteKey_Init(key_info_t* key_info)		//将PS2上面所有的键值映射到遥控器按键结构体中
+void RemoteKey_Info_Init(key_info_t* key_info)		//Map every key value on the remote control to the structure
 {
 //	HAL_SPI_Receive_DMA(&hspi1, Rx_Data, COMMUNI_CYCLE_LENGTH);
 	
+	key_info->MODE = Rx_Data[1];
 	key_info->LEFT = (Rx_Data[3] >> 7) & 0x01;
 	key_info->DOWN = (Rx_Data[3] >> 6) & 0x01;
 	key_info->RIGHT = (Rx_Data[3] >> 5) & 0x01;
@@ -38,7 +39,49 @@ void RemoteKey_Init(key_info_t* key_info)		//将PS2上面所有的键值映射到遥控器按键
 	key_info->L2 = Rx_Data[4] & 0x01;
 }
 
-void RemoteJoystick_Init(joystick_info_t* joystick_info)
+/**@brief Initialize joystick info of remote control.
+	*@param Joystick info structure ptr
+  *@retval None
+  */
+void RemoteJoystick_Info_Init(joystick_info_t* joystick_info)
 {
+	joystick_info->PSS_RX = Rx_Data[5];
+	joystick_info->PSS_RY = Rx_Data[6];
+	joystick_info->PSS_LX = Rx_Data[7];
+	joystick_info->PSS_LY = Rx_Data[8];
+}
+
+/**@brief Initialize remote control info.
+	*@param remote control info structure ptr
+  *@retval None
+  */
+void Remote_Info_Init(remote_info_t* remote_info)
+{
+	RemoteKey_Info_Init(remote_info->key_info);
+	RemoteJoystick_Info_Init(remote_info->joystick_info);
+}
+
+/**@brief Initialize Tx_Data from micro.
+	*@param None
+  *@retval None
+  */
+void Remote_Receive_Info_Init(void)		//MOSI
+{
+	Tx_Data[0] = CMD_START;
+	Tx_Data[1] = REQUEST_DATA;
 	
+	for(uint8_t i = 2; i < COMMUNI_CYCLE_LENGTH; i++)
+	{
+		Tx_Data[i] = 0x00;
+	}
+}
+
+/**@brief Start communication between micro and remote control.
+	*@param None
+  *@retval None
+  */
+void Remote_Communi_Start(void)
+{
+	Remote_Receive_Info_Init();
+	HAL_SPI_TransmitReceive_DMA(&hspi1, Tx_Data, Rx_Data, COMMUNI_CYCLE_LENGTH);
 }
--- Application/DeviceLayer/remote.h
+++ Application/DeviceLayer/remote.h
@@ -6,19 +6,21 @@
 
 /******************************Private Define**********************************************/
 
-#define 		PSS_X_LEFT_LIMIT				0x00
-#define 		PSS_X_RIGHT_LIMIT				0xFF
-#define 		PSS_Y_UP_LIMIT					0x00
-#define 		PSS_Y_DOWN_LIMIT				0xFF
-#define			PSS_MIDDLE							
+#define 		PSS_X_LEFT_LIMIT				0x03
+#define 		PSS_X_RIGHT_LIMIT				0xFD
+#define 		PSS_Y_UP_LIMIT					0x02
+#define 		PSS_Y_DOWN_LIMIT				0xFC
+#define 		PSS_X_MID	   						0X80
+#define 		PSS_Y_MID     					0X7F						
 
 /**@brief Mode switch of remote control.
   */
 #define			RED_MODE								0x73
 #define			GREEN_MODE							0x41
 
-#define			QUERY_MODE							0x01
-
+#define			CMD_START								0x01
+#define			REQUEST_DATA						0x42
+#define			SEND_START_FLAG					0x5A
 #define			COMMUNI_CYCLE_LENGTH		0x09
 
 /******************************Private Structure******************************************/
@@ -70,9 +72,16 @@ typedef struct
 typedef struct
 {
 	remote_info_t* remote_info;
-	uint8_t receive_request;		//micro sends 0x42 requesting data
-	uint8_t send_flag;					//remote control sends 0x5A to micro for confirmation before sending next byte of data
+//	uint8_t cmd_start;					//micro sends 0x01 for starting a communication cycle
+//	uint8_t receive_request;		//micro sends 0x42 requesting data
+//	uint8_t send_flag;					//remote control sends 0x5A to micro for confirmation before sending next byte of data
 	
 }remote_t;
+
+void RemoteKey_Info_Init(key_info_t* key_info);
+void RemoteJoystick_Info_Init(joystick_info_t* joystick_info);
+void Remote_Info_Init(remote_info_t* remote_info);
+void Remote_Receive_Info_Init(void);		//MOSI
+void Remote_Communi_Start(void);
 
 #endif
